62ae2b98eef470714ed1a057882e1267a91eae3a
prasanjit101 <prasanjitdutta45@gmail.com>
2024-07-29 16:48:14 +0530

    number of retrieved files changed to 2

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -13,7 +13,7 @@ export const POST = async (req: NextRequest, res: any) => {
 		const embeddingModel = getEmbeddingsModel();
 		const embedding = await embeddingModel.embedQuery(searchText);
 		const records = await pineconeIndex.query({
-			topK: 10,
+			topK: 2,
 			vector: embedding,
 			includeMetadata: true,
 			includeValues: false,
